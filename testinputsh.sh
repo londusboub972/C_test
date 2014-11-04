e5cb30d87e9818e2cf104b241c89974e757768c1
Simplonco <you@example.com>
2014-11-04 12:18:26 +0100

    add exit when user done the push

--- testinputsh.sh
+++ testinputsh.sh
@@ -2,7 +2,7 @@ git commit -a
 while true; do
     read -p "Do you want to push it now ?" yn
     case $yn in
-        [Yy]* ) git push origin master;;
+        [Yy]* ) git push origin master && exit;;
         [Nn]* ) echo "All done think to push it bro! " && exit;;
         * ) echo "Answers Yy or Nn";;
     esac
